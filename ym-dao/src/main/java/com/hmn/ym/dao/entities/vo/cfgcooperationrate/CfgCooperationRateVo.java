0391ff0cdb29fb0817ec5f6b1d9beb7e1a767c8a
xufuzhou <753911752@qq.com>
2019-09-01 23:02:32 +0800

    初始化项目

--- ym-dao/src/main/java/com/hmn/ym/dao/entities/vo/cfgcooperationrate/CfgCooperationRateVo.java
+++ ym-dao/src/main/java/com/hmn/ym/dao/entities/vo/cfgcooperationrate/CfgCooperationRateVo.java
@@ -1,22 +1,13 @@
 package com.hmn.ym.dao.entities.vo.cfgcooperationrate;
 
-import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.*;
+import com.hmn.ym.dao.entities.vo.BaseVo;
+import lombok.Data;
 
-import lombok.Getter;
-import lombok.Setter;
-@Table(name = "t_cfg_cooperation_rate")
-@Getter@Setter
-public class CfgCooperationRateVo implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+@Data
+public class CfgCooperationRateVo extends BaseVo {
     /**
      * 合作者用户ID 如医院id 店面id
      */
-    @Column(name = "amortization_id")
     private Long amortizationId;
 
     private Byte status;
@@ -32,30 +23,7 @@ public class CfgCooperationRateVo implements Serializable {
     private Byte type;
 
     /**
-     * 创建时间
-     */
-    @Column(name = "create_time")
-    private Date createTime;
-
-    @Column(name = "update_time")
-    private Date updateTime;
-
-    /**
      * 备注信息
      */
     private String remark;
-
-    /**
-     * 更新者
-     */
-    @Column(name = "update_id")
-    private Long updateId;
-
-    /**
-     * 创建者
-     */
-    @Column(name = "create_id")
-    private Long createId;
-
-    private static final long serialVersionUID = 1L;
 }
