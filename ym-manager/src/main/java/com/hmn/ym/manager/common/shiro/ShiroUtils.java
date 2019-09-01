5b7d428a7df54f7a63ce22c342cc4814233ce69f
xufuzhou <753911752@qq.com>
2019-09-01 20:16:02 +0800

    初始化项目

--- ym-manager/src/main/java/com/hmn/ym/manager/common/shiro/ShiroUtils.java
+++ ym-manager/src/main/java/com/hmn/ym/manager/common/shiro/ShiroUtils.java
@@ -1,8 +1,7 @@
 package com.hmn.ym.manager.common.shiro;
 
-import com.hmn.ym.dao.entities.po.Admin;
+import com.hmn.ym.dao.entities.vo.ShiroUser;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 
 
 public final class ShiroUtils {
@@ -12,12 +11,12 @@ public final class ShiroUtils {
      *
      * @return
      */
-    public static Admin getUser() {
-        Subject subject = SecurityUtils.getSubject();
+    public static ShiroUser getUser() {
+        ShiroUser subject = (ShiroUser) SecurityUtils.getSubject();
         if (subject == null) {
             return null;
         }
-        return (Admin) subject.getSession().getAttribute("user");
+        return subject;
     }
 
     /**
@@ -26,11 +25,11 @@ public final class ShiroUtils {
      * @return
      */
     public static Integer getUserId() {
-        Subject subject = SecurityUtils.getSubject();
+        ShiroUser subject = (ShiroUser) SecurityUtils.getSubject();
         if (subject == null) {
             return null;
         }
-        return (Integer) subject.getSession().getAttribute("userId");
+        return subject.getId();
     }
 
     /**
@@ -39,10 +38,10 @@ public final class ShiroUtils {
      * @return
      */
     public static String getUserName() {
-        Subject subject = SecurityUtils.getSubject();
+        ShiroUser subject = (ShiroUser) SecurityUtils.getSubject();
         if (subject == null) {
             return null;
         }
-        return (String) subject.getSession().getAttribute("userName");
+        return subject.getName();
     }
 }
