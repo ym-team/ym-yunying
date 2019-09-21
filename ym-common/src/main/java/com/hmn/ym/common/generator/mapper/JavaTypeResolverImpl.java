0024191628b52722f860c551869ee790755ad0e8
xufuzhou <753911752@qq.com>
2019-09-21 11:42:15 +0800

    初始化项目

---
+++ ym-common/src/main/java/com/hmn/ym/common/generator/mapper/JavaTypeResolverImpl.java
@@ -0,0 +1,15 @@
+package com.hmn.ym.common.generator.mapper;
+
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
+
+import java.sql.Types;
+
+public class JavaTypeResolverImpl extends JavaTypeResolverDefaultImpl {
+
+    public JavaTypeResolverImpl() {
+        super();
+        //把数据库的 TINYINT 映射成 Integer
+        super.typeMap.put(Types.TINYINT, new JdbcTypeInformation("TINYINT", new FullyQualifiedJavaType(Integer.class.getName())));
+    }
+}
