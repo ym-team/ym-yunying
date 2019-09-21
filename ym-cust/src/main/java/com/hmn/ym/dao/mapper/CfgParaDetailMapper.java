eeaaf22df38aa033c9979b5cd08e9a7ef7d339e8
xufuzhou <753911752@qq.com>
2019-09-21 11:23:14 +0800

    初始化项目

--- ym-cust/src/main/java/com/hmn/ym/dao/mapper/CfgParaDetailMapper.java
+++ ym-cust/src/main/java/com/hmn/ym/dao/mapper/CfgParaDetailMapper.java
@@ -3,9 +3,5 @@ package com.hmn.ym.dao.mapper;
 import com.hmn.ym.dao.entity.po.CfgParaDetail;
 import tk.mybatis.mapper.common.Mapper;
 
-import java.util.List;
-
 public interface CfgParaDetailMapper extends Mapper<CfgParaDetail> {
-
-    List<CfgParaDetail> selectCfgParaDetailEntityList();
 }
