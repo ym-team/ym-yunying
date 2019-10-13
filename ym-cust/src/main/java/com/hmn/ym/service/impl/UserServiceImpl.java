a613bcf6cba54e3e50eedbbae245c32e6419a05c
xufuzhou <753911752@qq.com>
2019-10-13 16:06:57 +0800

    初始化项目

--- ym-cust/src/main/java/com/hmn/ym/service/impl/UserServiceImpl.java
+++ ym-cust/src/main/java/com/hmn/ym/service/impl/UserServiceImpl.java
@@ -46,7 +46,7 @@ public class UserServiceImpl extends BaseServiceImpl<User, UserMapper> implement
         saleMan.setUserId(user.getId());
         saleMan.setPhone(user.getPhone());
         saleMan.setAuditStatus(2);
-        saleMan.setLevel("美业导师");
+        saleMan.setLevel("美业学员");
         saleMan.setCreateTime(new Date());
         saleMan.setUpdateId(user.getId());
         saleMan.setUpdateTime(new Date());
